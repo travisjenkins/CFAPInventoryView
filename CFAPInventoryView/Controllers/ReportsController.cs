b38a123c29e4015dcb88df905e3afe11b8df650f
Travis Jenkins <Tjinks123@gmail.com>
2023-12-10 13:23:35 -0600

    Updated error handling for Statistics page.

--- CFAPInventoryView/Controllers/ReportsController.cs
+++ CFAPInventoryView/Controllers/ReportsController.cs
@@ -27,15 +27,15 @@ namespace CFAPInventoryView.Controllers
         public async Task<IActionResult> Statistics()
         {
             StatisticsViewModel viewModel = new();
+            // Supplies
             try
             {
-                // Supplies
                 var supplies = await _context.Supplies.AsNoTracking()
-                                                      .Where(s => s.Active)
-                                                      .ToListAsync();
+                                                    .Where(s => s.Active)
+                                                    .ToListAsync();
                 var supplyTransactions = await _context.SupplyTransactions.AsNoTracking()
-                                                                          .Include(st => st.Supply)
-                                                                          .ToListAsync();
+                                                                            .Include(st => st.Supply)
+                                                                            .ToListAsync();
                 viewModel.TotalItemsThatExpire = supplies.Where(s => s.Expires).Count();
                 // Calculate total supply inventory price and average duration on shelf
                 if (supplies is not null && supplies.Count > 0)
@@ -49,7 +49,7 @@ namespace CFAPInventoryView.Controllers
                     }
                     viewModel.TotalSupplies = totalSupplies;
                     viewModel.TotalInventoryPrice = totalInventoryPrice;
-                    
+
                     long temp = 0;
                     foreach (var transaction in supplyTransactions)
                     {
@@ -60,8 +60,22 @@ namespace CFAPInventoryView.Controllers
                     }
                     viewModel.AverageSupplyDurationOnShelf = new TimeSpan(temp);
                 }
+            }
+            catch (DbException ex)
+            {
+                _logger.LogError($"ERROR:  {ex.Message}, StackTrace:  {ex.StackTrace}");
+                ModelState.AddModelError(string.Empty, "ERROR: There was an issue retrieving supply information from the database. If the issue persists, please contact and administrator.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERROR:  {ex.Message}, StackTrace:  {ex.StackTrace}");
+                ModelState.AddModelError(string.Empty, "ERROR: Something bad happened calculating supply information. If the issue persists, please contact and administrator.");
+            }
 
-                // iBelong
+
+            // iBelong
+            try
+            {
                 decimal totalIBelongBasketPrice = 0;
 #pragma warning disable 8604
                 var baskets = await _context.Baskets.AsNoTracking()
@@ -71,11 +85,11 @@ namespace CFAPInventoryView.Controllers
                                                     .ToListAsync();
 #pragma warning restore 8604
                 var basketTransactions = await _context.BasketTransactions.AsNoTracking()
-                                                                          .Include(bt => bt.Basket)
-                                                                          .ToListAsync();
+                                                                            .Include(bt => bt.Basket)
+                                                                            .ToListAsync();
                 viewModel.TotalShoppingListBaskets = await _context.Baskets.AsNoTracking()
-                                                                           .Where(b => b.Active && b.IsShoppingListItem)
-                                                                           .CountAsync();
+                                                                            .Where(b => b.Active && b.IsShoppingListItem)
+                                                                            .CountAsync();
                 viewModel.TotaliBelongBaskets = baskets.Count;
                 if (baskets is not null && baskets.Count > 0)
                 {
@@ -94,41 +108,69 @@ namespace CFAPInventoryView.Controllers
                     viewModel.AverageBasketDurationOnShelf = new TimeSpan(temp);
                 }
                 viewModel.TotaliBelongBasketPrice = totalIBelongBasketPrice;
+            }
+            catch (DbException ex)
+            {
+                _logger.LogError($"ERROR:  {ex.Message}, StackTrace:  {ex.StackTrace}");
+                ModelState.AddModelError(string.Empty, "ERROR: There was an issue retrieving iBelong information from the database. If the issue persists, please contact and administrator.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERROR:  {ex.Message}, StackTrace:  {ex.StackTrace}");
+                ModelState.AddModelError(string.Empty, "ERROR: Something bad happened calculating iBelong information. If the issue persists, please contact and administrator.");
+            }
 
-                // Donors
+
+            // Donors
+            try
+            {
                 var donors = await _context.Donors.AsNoTracking()
-                                                  .Include(d => d.SuppliesDonated)
-                                                  .ToListAsync();
+                                                .Include(d => d.SuppliesDonated)
+                                                .ToListAsync();
                 viewModel.TopNumberOfDonations = donors.Max(d => d.SuppliesDonated.Count);
                 viewModel.TopDonorByNumberOfDonations = donors.FirstOrDefault(d => d.SuppliesDonated.Count == viewModel.TopNumberOfDonations);
                 viewModel.TopDollarAmountDonated = donors.Max(d => d.TotalOfDonations);
                 viewModel.TopDonorByDollarAmountDonated = donors.FirstOrDefault(d => d.TotalOfDonations == viewModel.TopDollarAmountDonated);
+            }
+            catch (DbException ex)
+            {
+                _logger.LogError($"ERROR:  {ex.Message}, StackTrace:  {ex.StackTrace}");
+                ModelState.AddModelError(string.Empty, "ERROR: There was an issue retrieving donor information from the database. If the issue persists, please contact and administrator.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERROR:  {ex.Message}, StackTrace:  {ex.StackTrace}");
+                ModelState.AddModelError(string.Empty, "ERROR: Something bad happened calculating donor information. If the issue persists, please contact and administrator.");
+            }
 
-                // Recipients
+
+            // Recipients
+            try
+            {
                 var recipients = await _context.Recipients.AsNoTracking()
-                                                          .Include(r => r.SupplyTransactions)
+                                                        .Include(r => r.SupplyTransactions)
                                                             .ThenInclude(st => st.Supply)
-                                                          .Include(r => r.BasketTransactions)
+                                                        .Include(r => r.BasketTransactions)
                                                             .ThenInclude(bt => bt.Basket)
-                                                          .ToListAsync();
+                                                        .ToListAsync();
                 viewModel.TopNumberOfItemsReceived = recipients
                     .Max(r => r.SupplyTransactions.Count + r.BasketTransactions.Count);
                 viewModel.TopRecipientByNumberOfItemsReceived = recipients
                     .FirstOrDefault(r => (r.SupplyTransactions.Count + r.BasketTransactions.Count) == viewModel.TopNumberOfItemsReceived);
                 viewModel.TopDollarAmountReceived = recipients
-                    .Max(r => r.SupplyTransactions.Max(st => st.Supply.TotalPrice) + r.BasketTransactions.Max(bt => bt.Basket.TotalPrice));
+                    .Max(r => r.TotalOfDonationsReceived);
                 viewModel.TopRecipientByDollarAmountReceived = recipients
-                    .FirstOrDefault(r => (r.SupplyTransactions.Max(st => st.Supply.TotalPrice) + r.BasketTransactions.Max(bt => bt.Basket.TotalPrice)) == viewModel.TopDollarAmountReceived);
+                    .FirstOrDefault(r => r.TotalOfDonationsReceived == viewModel.TopDollarAmountReceived);
             }
             catch (DbException ex)
             {
                 _logger.LogError($"ERROR:  {ex.Message}, StackTrace:  {ex.StackTrace}");
-                ModelState.AddModelError(string.Empty, "ERROR: There was an issue retrieving information from the database. If the issue persists, please contact and administrator.");
+                ModelState.AddModelError(string.Empty, "ERROR: There was an issue retrieving recipient information from the database. If the issue persists, please contact and administrator.");
             }
             catch (Exception ex)
             {
                 _logger.LogError($"ERROR:  {ex.Message}, StackTrace:  {ex.StackTrace}");
-                ModelState.AddModelError(string.Empty, "ERROR: Something bad happened. If the issue persists, please contact and administrator.");
+                ModelState.AddModelError(string.Empty, "ERROR: Something bad happened calculating recipient information. If the issue persists, please contact and administrator.");
             }
             return View(viewModel);
         }
